68ce0605cea154b968c0e8b05666147800c29bff
andré <andre-haubert@educar.rs.gov.br>
2024-09-01 21:26:06 -0300

    avanços

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
-const app = require("../src/api");
+const app = require("../src/api");      
 
-app.use((req, res, next)=>{
-    next();  
+app.use((req, res, next) => {
+    next();
 });
 
-let port = process.env.API_PORT || 5000;
+let port = process.env.API_PORT || 3000;
 
-app.listen(port);
-
-console.log("Starting in port " + port)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    console.log(`DB HOST: ${process.env.DB_HOST}`);
+});
